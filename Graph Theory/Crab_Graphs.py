08fd586f57bc3a03c7a5b28e615e901f7fd95eac
czqzju <czqzju@gmail.com>
2019-03-18 14:16:09 +0800

    finished Crab_Graphs

--- Graph Theory/Crab_Graphs.py
+++ Graph Theory/Crab_Graphs.py
@@ -1,43 +1,71 @@
 #!/bin/python3
+#https://www.hackerrank.com/challenges/crab-graphs/problem
 
-import os
 import sys
 
 #
 # Complete the crabGraphs function below.
 #
+def max_flow(s, t, cap):
+    parent = [-1] * (t + 1)
+    maxflow = 0
+    cnt = 0
+
+    while BFS(s, t, cap, parent):
+        pathFlow = sys.maxsize
+        v = t
+        while not v is s:
+            u = parent[v]
+            pathFlow = cap[u][v] if cap[u][v] < pathFlow else pathFlow
+            v = u
+
+        v = t
+        while not v is s:
+            u = parent[v]
+            cap[u][v] -= pathFlow
+            cap[v][u] += pathFlow
+            v = u
+        maxflow += pathFlow
+        cnt += 1
+    print(cnt)
+    return maxflow
+
+def BFS(s, t, cap, parent) :
+    visited = [False] * (t + 1)
+    q = list()
+    q.append(s)
+    visited[s] = True
+    parent[s] = -1
+    while len(q) > 0:
+        cur = q.pop()
+        for i in range(0, t + 1):
+            if not visited[i] and cap[cur][i] > 0 :
+                q.append(i)
+                visited[i] = True
+                parent[i] = cur
+    return visited[t] is True
+
+
 def crabGraphs(n, t, graph):
-    edges = dict()
-    countA = [0] * n
-    countB = [1] * n
-    res = 0
+
+    cap = [[0] * (n*2+2) for _ in range(0, n *2 + 2)]
+    s = n * 2
+    des = s + 1
+
     for i in range(0, len(graph)):
         v1 = graph[i][0] - 1
         v2 = graph[i][1] - 1
-        if v1 in edges:
-            edges[v1].add(v2)
-        else:
-            edges[v1] = set()
-            edges[v1].add(v2)
-        countA[v1] += 1
 
-
-        if v2 in edges:
-            edges[v2].add(v1)
-        else:
-            edges[v2] = set()
-            edges[v2].add(v1)
-        countA[v2] += 1
-
+        cap[2 * v1][2 * v2 + 1] = 1
+        cap[2 * v2][2 * v1 + 1] = 1
+        cap[s][2 * v1] += 1
+        cap[s][2 * v2] += 1
     for i in range(0, n):
-        if not countA[i]: continue
-        for v in edges[i]:
-            if countB[v]:
-                countA[i] -= 1
-                countB[v] -= 1
-                res += 1
-                if not countA[i]: break
-    return res
+        cap[2 * i + 1][des] = 1
+        cap[s][2 * i] = t if cap[s][2 * i] > t else cap[s][2 * i]
+
+    # print(sum(cap[i][101] for i in range(0, 102)))
+    return max_flow(s, des, cap)
 
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
