f90f0cd54500115d5aa94b057400e18d3565ce6f
czqzju <czqzju@gmail.com>
2019-03-27 19:31:58 +0800

    update Goodland_Electricity

--- Graph Theory/Goodland_Electricity.py
+++ Graph Theory/Goodland_Electricity.py
@@ -10,9 +10,9 @@ import sys
 def pylons(k, arr):
     ret = 0
     cur = 0
-    while cur < n:
+    while cur < len(arr):
         found = False
-        for i in range(k - 1, 0, -1):
+        for i in range(k - 1, -1, -1):
             if cur + i >=n: continue
             if arr[cur + i] == 1:
                 found = True
